4348960b7db20b5ec04d3d2b8e1960265b0f513d
monlover <113494693+monlover@users.noreply.github.com>
2025-05-13 14:28:12 +0900

    Delete AIclass_hw directory

--- AIclass_hw/script.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.menu a, footer a').forEach(link => {
-    link.addEventListener('click', (e) => {
-      console.log(`${e.target.textContent} 버튼이 클릭되었습니다.`);
-    });
-  });
-  
